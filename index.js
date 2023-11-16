2fb32464bfcb6cb01fe4711077d403df6aca08b7
BrandyW <95672986+brandywalch@users.noreply.github.com>
2023-11-16 10:39:31 -0800

    Refactor existing code

--- index.js
+++ index.js
@@ -1,53 +1,41 @@
-// Function to return either rock, paper or scissors at random.
-// Create variable to hold random number
-// Use Math to return either 0,1 2 at random
-// If statement for rock,paper,scissors
-// CALL function
+const options = ["rock", "paper", "scissors"]
 
-function getComputerChoice(){
-    const randomNum = Math.floor(Math.random() * 3)
-    if (randomNum === 0) {
-        return "Rock"
-    } else if (randomNum === 1) {
-        return "Paper"
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random() * options.length)]
+    return(choice)
+}
+
+function checkWinner (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "tie"
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")) {
+        return "player"
     } else {
-        return "Scissors"
+        return "computer"
     }
 }
 
-// console.log(getComputerChoice())
-
-// return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-// Use if statements to delcare winner
-
-let playerSelection ="Scissors"
-const computerSelection = getComputerChoice()
-
 function playRound(playerSelection, computerSelection) {
-    
-    if (playerSelection === "Rock" && computerSelection === "Paper") {
-        return "You Lose! Paper Beats Rock!"
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return "You Win! Rock Beats Scissors"
-    } else if (playerSelection === "Rock" && computerSelection === "Rock") {
-        return "Its a Tie!"
-
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        return "You Lose! Scissors Beats Paper"
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        return "You Win! Paper Beats Rock"
-    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+    const result = checkWinner (playerSelection, computerSelection)
+    if (result === "tie") {
         return "It's a Tie!"
-    } 
-
-    else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return "You Lose! Rock Beats Scissors"
-    }  else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        return "You Win! Scissors Beats Paper"
-    }  else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-        return "It's a Tie!"
-    } 
+    } else if (result === "player") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = "rock" 
+        const computerSelection = getComputerChoice()
+        console.log(playRound(playerSelection, computerSelection))
+    }
+ }
 
-console.log(playRound(playerSelection, computerSelection))
+game()
+
